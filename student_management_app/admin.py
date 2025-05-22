ced6f2d5bc6e3b43465bc13f8cd2fb9ef2e8fe63
CSE for VNP <136301142+NguyenGiap25072004@users.noreply.github.com>
2025-05-22 14:46:34 +0700

    Giap - Update admin.py

--- student_management_app/admin.py
+++ student_management_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, AdminHOD, Staffs, Courses, Subjects, Students, Attendance, AttendanceReport, LeaveReportStudent, LeaveReportStaff, FeedBackStudent, FeedBackStaffs, NotificationStudent, NotificationStaffs
-
+from .models import TodoItem # Thêm import này
 # Register your models here.
 class UserModel(UserAdmin):
     pass
@@ -22,3 +22,4 @@ admin.site.register(FeedBackStudent)
 admin.site.register(FeedBackStaffs)
 admin.site.register(NotificationStudent)
 admin.site.register(NotificationStaffs)
+admin.site.register(TodoItem)
